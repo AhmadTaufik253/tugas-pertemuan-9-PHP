42026226e858115e2d7efa58eb4679ef25797d72
AhmadTaufik253 <ahmadtaufik2503@gmail.com>
2023-04-05 07:45:53 +0700

    tugas pertemuan 9 php

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+include_once 'top.php';
+include_once 'menu.php';
+?>
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+            <h1 class="mt-4">Contact</h1>
+            <ol class="breadcrumb mb-4">
+                <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
+                <li class="breadcrumb-item active">Contact</li>
+            </ol>
+            <div class="card mb-4">
+                <div class="card-body">
+                    <p class="mb-0">
+                    
+                    </p>
+                </div>
+            </div>
+        </div>
+    </main>
+<?php
+include_once 'bottom.php';
+?>
